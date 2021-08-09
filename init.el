6e4100a366ff41fe61a287829f2c012d66c8aada
William Chen <chenwilliamy77@gmail.com>
2021-08-09 10:07:12 -0400

    Add init.el

---
+++ init.el
@@ -0,0 +1,24 @@
+;; Load auto math complete
+(load auto-complete-auctex)
+
+;; Load package for auto math complete
+(require 'auto-complete-auctex)
+
+(require 'package)
+(package-initialize)
+(require 'auto-complete-config)
+(ac-config-default)
+
+;; Activate auto-complete for latex modes (AUCTeX or Emacs' builtin one).
+(add-to-list 'ac-modes 'latex-mode)
+
+;; Activate ac-math.
+(eval-after-load "latex"
+  '(when (featurep 'auto-complete)
+     ;; See https://github.com/vspinu/ac-math
+     (require 'ac-math)
+     (defun ac-latex-mode-setup ()       ; add ac-sources to default ac-sources
+       (setq ac-sources
+         (append '(ac-source-math-unicode ac-source-math-latex ac-source-latex-commands)
+             ac-sources)))
+     (add-hook 'LaTeX-mode-hook 'ac-latex-mode-setup)))
